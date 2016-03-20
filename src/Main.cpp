a3ab36d0c739811436d1f2906c45d76c2a3bdcb7
cheunggg <cheunggg@github.com>
2016-03-19 23:25:51 -0600

    removed unnecessary files

--- src/Main.cpp
+++
@@ -1,9 +0,0 @@
-int main() {
-
-    Ben ben = Ben();
-    Gordon gordon = Gordon();
-    gordon.add_listener(ben);
-    gordon.stumped_toe();
-    
-    return 0;
-}
--- src/ben.cpp
+++
@@ -1,9 +0,0 @@
-#include "ben.h"
-#include <iostream>
-
-Ben::Ben() {}
-
-void Ben::heard_something()
-{
-    std::cout << "Ben: I think I heard something" << std::endl;
-}
