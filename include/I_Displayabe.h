2d897326c45577d56d297cd914a0c6166ac35f45
cheunggg <cheunggg@github.com>
2016-03-26 16:27:47 -0600

    Interface displayable returns a texture and takes in a SDL_Renderer

--- include/I_Displayabe.h
+++ include/I_Displayabe.h
@@ -1,7 +1,15 @@
 #pragma once
+#include <SDL2/SDL.h>
+
+// in main add every displayable object to an displayable_array
+// for each object in displayable_array call the display() function
+//      i) DESIGN CHOICE 1
+//      Pass renderer to every displayable object, and RenderCopy() texture to renderer
+//      ii) DESIGN CHOICE 2
+//      display will return a texture where it will be RenderCopy() inside the loop (in main)
 
 class I_Displayable 
 {
 public:
-    virtual void display();
+    virtual SDL_Texture display(SDL_Renderer *renderer);
 };
