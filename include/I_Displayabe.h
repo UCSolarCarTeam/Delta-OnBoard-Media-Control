69edb98e21183cf1d54ba3e589d3454271eba224
cheunggg <cheunggg@github.com>
2016-03-26 16:31:27 -0600

    Edited comments in displayable

--- include/I_Displayabe.h
+++ include/I_Displayabe.h
@@ -4,8 +4,6 @@
 // in main add every displayable object to an displayable_array
 // for each object in displayable_array call the display() function
 //      i) DESIGN CHOICE 1
-//      Pass renderer to every displayable object, and RenderCopy() texture to renderer
-//      ii) DESIGN CHOICE 2
 //      display will return a texture where it will be RenderCopy() inside the loop (in main)
 
 class I_Displayable 
