0b9f98859b7765313b30f4899a1e99591be888a6
Gordon Cheung <gordoncheung2@gmail.com>
2016-06-04 11:43:06 -0600

    Change argument for CreateRenderer from -1 to 0

--- src/SDL.cpp
+++ src/SDL.cpp
@@ -27,7 +27,7 @@ bool SDL::init_SDL()
         }
         else
         {
-            renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+            renderer = SDL_CreateRenderer(window, 0, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
             if (renderer == NULL)
             {
                 printf("Renderer could not be created. SDL_Error: %s \n", SDL_GetError());
