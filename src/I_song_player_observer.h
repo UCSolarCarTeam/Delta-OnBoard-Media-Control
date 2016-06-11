bac08b0bb0ed19eb07e69322a330a42bb4321cec
Ben Graham <benjamin.graham@ucalgary.ca>
2016-06-11 11:53:08 -0600

    added vulme and time to observer

--- src/I_song_player_observer.h
+++ src/I_song_player_observer.h
@@ -5,4 +5,8 @@ class I_SongPlayerObserver
 {
 public:
     virtual void current_song(std::string song_path) = 0;
+
+    virtual void current_volume(double volume) = 0; 
+
+    virtual void current_time(double time) = 0;
 };
