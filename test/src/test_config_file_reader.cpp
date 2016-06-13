4c6bf65c87c551a9dd8931763d09a20c0fb3025a
Gordon Cheung <gordoncheung2@gmail.com>
2016-06-13 17:37:04 -0600

    Debug config file reader tests.

--- test/src/test_config_file_reader.cpp
+++ test/src/test_config_file_reader.cpp
@@ -1,9 +1,11 @@
 #include "gtest/gtest.h"
 #include "gmock/gmock.h"
+#include <string>
 
 #include "ConfigFileReader.h"
 
 using ::testing::_;
+
 
 class ConfigFileReaderTest : public ::testing::Test {
     protected:
@@ -14,8 +16,9 @@ class ConfigFileReaderTest : public ::testing::Test {
 		}
 };
 
+ConfigFileReader testReader("./test_resources/config_file_reader/test.ini");
+
 TEST(ConfigFileReaderTest, getDoubleFalse) {
-	ConfigFileReader testReader("./test_resources/config_file_reader/test.ini"));
 	double expected = 20;
 	double actual = testReader.getDouble("DEFAULT", "GREET", 20);
 	EXPECT_EQ(expected, actual);
@@ -23,18 +26,18 @@ TEST(ConfigFileReaderTest, getDoubleFalse) {
 
 TEST(ConfigFileReaderTest, getDoubleTrue) {
 	double expected = 768;
-	double actual = testReader.getDouble("SCREEN". "SCREEN", 20);
+	double actual = testReader.getDouble("SCREEN", "SCREEN_HEIGHT", 20);
 	EXPECT_EQ(expected, actual);
 }
 
-TEST(ConfigFileReaderTest, getString)
-	std::string expected = 'SDL Screen';
-	std::string actual = reader.getString("SCREEN", "SCREEN_TITLE", "Title Screen");
+TEST(ConfigFileReaderTest, getString) {
+	std::string expected = "SDL Screen";
+	std::string actual = testReader.getString("SCREEN", "SCREEN_TITLE", "Title Screen");
 	EXPECT_EQ(expected, actual);
 }
 
 TEST(ConfigFileReaderTest, getBoolean) {
 	bool expected = false;
-	bool actual = reader.getBoolean("SCREEN", "SCREEN_ENABLE", true);
+	bool actual = testReader.getBoolean("SCREEN", "SCREEN_ENABLE", true);
 	EXPECT_EQ(expected, actual);
 }
